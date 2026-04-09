447abc6ca0e8aac5494908a1c2b64d74f94146c7
mahyarx <microtechnology@gmail.com>
2026-04-09 18:29:39 +0300

    fix terraform syntax

--- terraform/secrets.tf
+++ terraform/secrets.tf
@@ -1,20 +1,26 @@
 resource "google_secret_manager_secret" "senaite_admin_password" {
   secret_id = "senaite-admin-password"
-  replication { auto {} }
+  replication {
+      auto {}
+  }
 
   labels = local.common_labels
 }
 
 resource "google_secret_manager_secret" "smtp_password" {
   secret_id = "smtp-password"
-  replication { auto {} }
+  replication {
+      auto {}
+  }
 
   labels = local.common_labels
 }
 
 resource "google_secret_manager_secret" "tenant_registry" {
   secret_id = "tenant-registry"
-  replication { auto {} }
+  replication {
+      auto {}
+  }
 
   labels = local.common_labels
 }
